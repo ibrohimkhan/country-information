2260bce97691c4522223a50e40ff5a74c4ace9d7
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2024-05-23 20:39:03 +0500

    updated url in remote api service

--- app/src/main/java/com/kodeco/android/countryinfo/networking/RemoteApiService.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/networking/RemoteApiService.kt
@@ -4,6 +4,6 @@ import com.kodeco.android.countryinfo.model.Country
 import retrofit2.http.GET
 
 interface RemoteApiService {
-    @GET("/v3.1/all")
+    @GET("/v3.1/all?fields=name,flags,population,area,capital")
     suspend fun getAllCountries(): List<Country>
 }
