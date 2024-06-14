43a9f84ee72e7e269b7169a0b23c3d7eea53302a
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2024-06-15 03:06:28 +0500

    added version number info in About screen

--- app/src/main/java/com/kodeco/android/countryinfo/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/screens/about/AboutScreen.kt
@@ -3,6 +3,7 @@ package com.kodeco.android.countryinfo.ui.screens.about
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -17,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kodeco.android.countryinfo.BuildConfig
 import com.kodeco.android.countryinfo.R
 import com.kodeco.android.countryinfo.ui.components.AppBar
 
@@ -24,6 +26,8 @@ import com.kodeco.android.countryinfo.ui.components.AppBar
 fun AboutScreen(
     onBackClick: () -> Unit
 ) {
+    val versionName = BuildConfig.VERSION_NAME
+
     BackHandler {
         onBackClick()
     }
@@ -48,6 +52,14 @@ fun AboutScreen(
                 Text(
                     text = stringResource(R.string.about_app_text),
                     style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = "Application version: $versionName",
+                    style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(16.dp)
                 )
             }
