1dd0072eaf0931b3ac87617f4341143eb08263a2
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2024-06-22 01:27:19 +0500

    added rotation animation for star icon on click event

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoRow.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoRow.kt
@@ -1,5 +1,7 @@
 package com.kodeco.android.countryinfo.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,14 +9,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,55 +40,67 @@ fun CountryInfoRow(
 ) {
     val unknown = stringResource(R.string.unknown)
 
-    Card(
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = MaterialTheme.shapes.medium
+            )
+            .clickable { clickAction(country) }
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .clickable { clickAction(country) }
+        Column(
+            modifier = Modifier.padding(8.dp)
         ) {
-            Column(
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = stringResource(R.string.name, country.name.common),
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(4.dp)
-                )
-                Text(
-                    text = stringResource(
-                        R.string.capital,
-                        country.capital?.firstOrNull() ?: unknown
-                    ),
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(4.dp)
-                )
-            }
-
-            FavoriteButton(
-                country = country,
-                onFavoriteClicked = onFavoriteClicked
+            Text(
+                text = stringResource(R.string.name, country.name.common),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = stringResource(
+                    R.string.capital,
+                    country.capital?.firstOrNull() ?: unknown
+                ),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(4.dp)
             )
         }
+
+        FavoriteButton(
+            country = country,
+            onFavoriteClicked = onFavoriteClicked
+        )
     }
 }
 
 @Composable
 fun FavoriteButton(country: Country, onFavoriteClicked: (Country) -> Unit) {
+    var isFavorite by remember { mutableStateOf(country.isFavorite) }
+
+    val rotationAnimation = animateFloatAsState(
+        label = "rotationAnimation",
+        targetValue = if (isFavorite) 360f else 0f
+    )
+
     val painter =
-        if (country.isFavorite) painterResource(id = R.drawable.star_filled)
+        if (isFavorite) painterResource(id = R.drawable.star_filled)
         else painterResource(id = R.drawable.star_outline)
 
-    IconButton(onClick = { onFavoriteClicked(country) }) {
+    IconButton(
+        onClick = {
+            isFavorite = !isFavorite
+            onFavoriteClicked(country)
+        }
+    ) {
         Icon(
             painter = painter,
             contentDescription = "Favorite",
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier
+                .size(24.dp)
+                .graphicsLayer(rotationZ = rotationAnimation.value)
         )
     }
 }
@@ -97,10 +115,27 @@ fun CountryInfoRowPreview() {
                 capital = listOf("Dushanbe"),
                 population = 10_000_000,
                 area = 300_000.0,
-                flags = CountryFlags("tjk.png")
+                flags = CountryFlags("tjk.png"),
             ),
             clickAction = {},
             onFavoriteClicked = {},
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavoriteButtonPreview() {
+    MyApplicationTheme {
+        FavoriteButton(
+            country = Country(
+                name = CountryName("Tajikistan"),
+                capital = listOf("Dushanbe"),
+                population = 10_000_000,
+                area = 300_000.0,
+                flags = CountryFlags("tjk.png")
+            ),
+            onFavoriteClicked = {}
         )
     }
 }
